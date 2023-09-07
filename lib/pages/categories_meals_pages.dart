8f51ce6a084cd5fbf2c3a38f5c0b2faa31ea6b28
Luana Dias <luana.dias@colab.re>
2023-09-07 17:47:58 -0300

    selecionando refeicoes por categorias

--- lib/pages/categories_meals_pages.dart
+++ lib/pages/categories_meals_pages.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dummy_data/data/dummy_data.dart';
 import 'package:flutter/material.dart';
 
 import 'package:dummy_data/models/category.dart';
@@ -13,11 +14,19 @@ class CategoriesMealsPages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)!.settings.arguments as Category;
+
+    final categoryMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList(); //retorna true ou false, se o item pertence a category
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(category.title),
-      ),
-      body: Text('Receitas por categoria ${category.id}'),
-    );
+        appBar: AppBar(
+          title: Text(category.title),
+        ),
+        body: ListView.builder(
+            itemCount: categoryMeals.length,
+            itemBuilder: (context, index) {
+              return Text(categoryMeals[index].title);
+            }));
   }
 }
