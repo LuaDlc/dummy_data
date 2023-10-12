e3e51dc7ad112a09f9025e5b252c3970c9d1bb78
Luana Dias <luana.dias@colab.re>
2023-10-12 20:41:28 -0300

    feature: tela de configuracoes com switch

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,19 +1,87 @@
 import 'package:flutter/material.dart';
 
+import '../models/settings.dart';
 import '../widgets/main_drawer.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
   @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
   Widget build(BuildContext context) {
+    var settings = Settings();
+    //funcao que retorna um switch
+    Widget _createSwitch(
+      String title,
+      String subtitle,
+      bool value,
+      Function(bool) onChanged,
+    ) {
+      return SwitchListTile.adaptive(
+          title: Text(title),
+          subtitle: Text(subtitle),
+          value: value,
+          onChanged:
+              onChanged); //adaptative pra definir comportamento diferente dependendo do sitema operacional
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Configuracoes'),
       ),
       drawer: const MainDrawer(),
-      body: Container(
-        child: const Text('Configuracoes'),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'Configuracoes',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _createSwitch(
+                  'sem Glutén',
+                  'só exibe refeicoes sem gluten',
+                  settings.isLactoseFree,
+                  (value) => setState(
+                    () => settings.isLactoseFree = value,
+                  ),
+                ),
+                _createSwitch(
+                  'Vegana',
+                  'só exibe refeicoes veganas',
+                  settings.isVegan,
+                  (value) => setState(
+                    () => settings.isVegan = value,
+                  ),
+                ),
+                _createSwitch(
+                  'Lactose',
+                  'só exibe refeicoes sem lactose',
+                  settings.isLactoseFree,
+                  (value) => setState(
+                    () => settings.isLactoseFree = value,
+                  ),
+                ),
+                _createSwitch(
+                  'Vegetariana',
+                  'só exibe refeicoes vegetarianas',
+                  settings.isVegetarian,
+                  (value) => setState(
+                    () => settings.isVegetarian = value,
+                  ),
+                ),
+              ],
+            ),
+          ), //usamos o listview sem bulder pois nao precisa criar tela, pois sera feito na mao uma lista exata
+        ],
       ),
     );
   }
